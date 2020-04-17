af5453e7db47323df97d8d2b32f2dd22d7e5d5eb
Sebastián Ávila Valdovinos <30665426+SebVald@users.noreply.github.com>
2020-04-17 01:52:22 -0500

    Agregada la pantalla Scrolling

---
+++ Practicas/app/src/main/java/com/example/dispositivosmoviles/data/model/Constantes.java
@@ -0,0 +1,5 @@
+package com.example.dispositivosmoviles.data.model;
+
+public class Constantes {
+    public static final String url= "http://www.lamarr.com.mx/webservice2.php";
+}
--- Practicas/app/src/main/java/com/example/dispositivosmoviles/ui/login/LoginActivity.java
+++ Practicas/app/src/main/java/com/example/dispositivosmoviles/ui/login/LoginActivity.java
@@ -30,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
